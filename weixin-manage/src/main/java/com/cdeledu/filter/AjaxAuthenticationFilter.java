109be2bcedd83f70171fd0aa02b44d99440078f7
皇族灬战狼 <duleilewuhen@163.com>
2016-06-07 20:33:50 +0800

    登录过滤器

--- weixin-manage/src/main/java/com/cdeledu/filter/AjaxAuthenticationFilter.java
+++ weixin-manage/src/main/java/com/cdeledu/filter/AjaxAuthenticationFilter.java
@@ -8,7 +8,11 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 
 import com.cdeledu.constant.Globals;
 import com.cdeledu.model.common.SessionInfo;
@@ -22,21 +26,57 @@ import com.cdeledu.util.ContextHolderUtils;
  * @since: JDK 1.7
  */
 public class AjaxAuthenticationFilter implements Filter {
+	/** ----------------------------------------------------- Fields start */
+	private static final Logger logger = Logger.getLogger(AjaxAuthenticationFilter.class);
+	/** ----------------------------------------------------- Fields end */
+	/**
+	    * 当前URI资源是否需要登录才能访问
+	    * @param requestURI
+	    * @param request
+	    * @return
+	    */
+		private boolean isURILogin(String requestURI, HttpServletRequest request) {
+			for (String uri : Globals.INHERENT_URIS) {
+				if (requestURI != null && requestURI.indexOf(uri) >= 0) {
+					return true;
+				}
+			}
+			return false;
+		}
+	
 
 	@Override
 	public void destroy() {
 
 	}
 
+	// ② 执行过滤
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
 			throws IOException, ServletException {
-		System.out.println("用于检查用户是否登录了系统，如果未登录，则重定向到指的登录页面。");
-		HttpSession session = ContextHolderUtils.getSession();
-		SessionInfo sessioninfo = (SessionInfo) session.getAttribute(Globals.USER_SESSION);
-		if (null != sessioninfo && null != sessioninfo.getManagerUser()) {
+		logger.info("用于检查用户是否登录了系统，如果未登录，则重定向到指的登录页面。");
+		// ②-1 保证该过滤器在一次请求中只被调用一次
+		if (null != request && null != request.getAttribute(Globals.FILTERED_REQUEST)) {
 			filterChain.doFilter(request, response);
 		} else {
+			// ②-2 设置过滤标识，防止一次请求多次过滤
+			request.setAttribute(Globals.FILTERED_REQUEST, Boolean.TRUE);
+			// ②-3 用户未登录, 且当前URI资源需要登录才能访问
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+			
+			HttpSession session = ContextHolderUtils.getSession();
+			SessionInfo sessioninfo = (SessionInfo) session.getAttribute(Globals.USER_SESSION);
+			if ((null == sessioninfo || null == sessioninfo.getManagerUser()) && isURILogin(httpRequest.getRequestURI(), httpRequest)) {
+				// ②-4将用户的请求URL保存在session中，用于登录成功之后，跳到目标URL
+				String toUrl = httpRequest.getRequestURL().toString();
+				if (!StringUtils.isEmpty(httpRequest.getQueryString())) {
+					toUrl += "?" + httpRequest.getQueryString();
+				}
+				logger.info("登录跳转页面"+toUrl);
+				// ②-5转发到登录页面
+				request.getRequestDispatcher("/webpage/login/login.jsp").forward(request, response);
+				return;
+			}
 			filterChain.doFilter(request, response);
 		}
 	}
