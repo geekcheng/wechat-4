14da1c192655fd19fa57c5e5f65e3c42b4fcc5a0
皇族灬战狼 <duleilewuhen@163.com>
2016-06-12 10:20:37 +0800

    浏览器类型

---
+++ weixin-base/src/main/java/com/cdeledu/type/BrowserType.java
@@ -0,0 +1,12 @@
+package com.cdeledu.type;
+
+/**
+ * @类描述: 浏览器类型
+ * @创建者: 皇族灬战狼
+ * @创建时间: 2016年6月12日 上午10:20:04
+ * @版本: V1.0
+ * @since: JDK 1.7
+ */
+public enum BrowserType {
+	IE10, IE9, IE8, IE7, IE6, Firefox, Safari, Chrome, Opera, Camino, Gecko
+}
