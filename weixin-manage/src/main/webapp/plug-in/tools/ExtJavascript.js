7b2ca0cb95d6b9cce3d55db9b0f01459a67cba87
皇族灬战狼 <duleilewuhen@163.com>
2016-06-29 20:27:21 +0800

    添加常用的js工具方法

--- weixin-manage/src/main/webapp/plug-in/tools/ExtJavascript.js
+++ weixin-manage/src/main/webapp/plug-in/tools/ExtJavascript.js
@@ -22,8 +22,86 @@ dllwh.getRootPath = function() {
 }
 
 /**
- * 判断是否为空
- * 不是一个空对象，或者未定义，或者不等于空字符串
+ * 公共工具类 <br/>
+ * http://www.htmleaf.com/ziliaoku/qianduanjiaocheng/201505311944.html
+ */
+var publicUtil = {
+	isNotEmpty : function(val) {
+		return !this.isEmpty(val);
+	},
+	isEmpty : function(val) {
+		if ((val == null || typeof (val) == "undefined")
+				|| (typeof (val) == "string" && val == "" && val != "undefined")) {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	// 去除元素内所有内容 strIds："#id1,#id2,#id3"
+	emptyHtml : function(strIds) {
+		try {
+			var ids = strIds.trim(",").split(",");
+			$(ids).each(function() {
+				var obj = $(this.toString());
+				if (obj.length > 0) {
+					$(obj).each(function() {
+						$(this).html("");
+					});
+				} else {
+					obj.html("");
+				}
+			});
+		} catch (ex) {
+			throw new Error("js方法:【publicUtil.emptyHtml(strIds)】,error！")
+		}
+	},
+	// 去除元素的值 strIds："#id1,#id2,#id3"
+	emptyValue : function(strIds) {
+		try {
+			var ids = strIds.trim(",").split(",");
+			$(ids).each(function() {
+				var obj = $(this.toString());
+				if (obj.length > 0) {
+					$(obj).each(function() {
+						$(this).val("");
+					});
+				} else {
+					obj.val("");
+				}
+			});
+		} catch (ex) {
+			if (PublicUtil.isDebug()) {
+				throw new Error("js方法:【publicUtil.emptyValue(strIds)】,error！");
+			}
+		}
+	},
+	// 去除Textarea内所有内容 strIds："#id1,#id2,#id3"
+	emptyTextarea : function(strIds) {
+		try {
+			var ids = strIds.trim(",").split(",");
+			$(ids).each(function() {
+				var obj = $(this.toString());
+				if (obj.length > 0) {
+					$(obj).each(function() {
+						$(this).empty();
+						$(this).val("");
+					});
+				} else {
+					obj.empty();
+					obj.val("");
+				}
+			});
+		} catch (ex) {
+			if (PublicUtil.isDebug()) {
+				throw new Error(
+						"js方法:【publicUtil.emptyTextarea(strIds)】,error！");
+			}
+		}
+	}
+}
+
+/**
+ * 判断是否为空 不是一个空对象，或者未定义，或者不等于空字符串
  */
 dllwh.isNull = function(exp) {
 	if (exp !== null && typeof (exp) !== undefined && exp !== '')
